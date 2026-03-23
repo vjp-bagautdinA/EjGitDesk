a16d876877b9eacb361df15f548cb9df2229fdf7
vjp-bagautdinA <barsanaliev01@informatica.iesvalledeljerteplasencia.es>
2026-03-23 18:03:22 +0100

    Tema11Ej6 (terminar)

--- Tema11Ej6/src/tema11ej6/Campaña.java
+++ Tema11Ej6/src/tema11ej6/Campaña.java
@@ -1,23 +1,93 @@
 package tema11ej6;
 
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
 import java.util.Set;
-
-
 
 public class Campaña {
     
     //Atributos
-    private Set<String> donaciones = new HashSet<>();
+    private Set<Donacion> donaciones;
     private String nombre;
     
     //Constructores
 
     public Campaña(String nombre) {
         this.nombre = nombre;
+        this.donaciones = new HashSet<>();
     }
     
     public Campaña() {
         this.nombre = "";
+        this.donaciones = new HashSet<>();
     }
+    
+    //Getters y Setters
+
+    public Set<Donacion> getDonaciones() {
+        return donaciones;
+    }
+
+    public void setDonaciones(Set<Donacion> donaciones) {
+        this.donaciones = donaciones;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    //Metodos
+    
+    public void añadirDonacion(Campaña SIP) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Introduzca su nombre:");
+        String nombre = pedirString();
+        System.out.println("Introduzca la cantidad que quiere donar");
+        int cantidad = pedirInt();
+        Donacion donacion = new Donacion(nombre, cantidad);
+        donaciones.add(donacion);       
+    }
+    
+    public void mostrarDonaciones(Campaña SIP) {
+        System.out.println(donaciones);
+    }
+    
+    public void mostrarDonacionesPorNombre (Campaña SIP) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Introduzca el nombre del donante.");
+        String nombre = input.nextLine();
+        Iterator<Donacion> it = donaciones.iterator();
+        Donacion aux;
+        while (it.hasNext()) {
+            aux = it.next();
+            if(aux.getNombre().equalsIgnoreCase(nombre)) {
+                System.out.println(aux);
+            }
+        }
+    }
+    
+    public static String pedirString() {
+        Scanner input = new Scanner(System.in);
+        String string = input.nextLine();
+        return string;
+    }
+    
+    public static int pedirInt() {
+        Scanner input = new Scanner(System.in);
+        int int1 = input.nextInt();
+        return int1;
+    }
+    
+    //toString
+
+    @Override
+    public String toString() {
+        return "Campa\u00f1a{" + "donaciones=" + donaciones + ", nombre=" + nombre + '}';
+    }
+    
 }
--- Tema11Ej6/src/tema11ej6/Tema11Ej6.java
+++ Tema11Ej6/src/tema11ej6/Tema11Ej6.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Tema11Ej6 {
     
-    public static void menu() { //Metodo principal que enseña al usuario las opciones y controla excepciones.
+    public static void menu(Campaña SIP) { //Metodo principal que enseña al usuario las opciones y controla excepciones.
         int eleccionMenu = -1; //Atributos para controlar excepciones y elegir opciones.
         boolean ready = false;
         Scanner input = new Scanner(System.in);
@@ -13,10 +13,13 @@ public class Tema11Ej6 {
             try {
                 System.out.println("-------------------------------------");//Las opciones del menu separadas por barras y controladas con un trycatch.
                 System.out.println("¿Que quieres hacer?");
-                System.out.println("1. .");
-                System.out.println("2. .");
-                System.out.println("3. .");
-                System.out.println("4. Salir.");
+                System.out.println("1.  Añadir donación.");
+                System.out.println("2. Mostrar donaciones.");
+                System.out.println("3. Mostrar donaciones por nombre de donante.");
+                System.out.println("4. Mostrar número de donaciones.");
+                System.out.println("5. Mostrar dinero total recaudado.");
+                System.out.println("6. Ordenar donaciones ");
+                System.out.println("7. Salir.");
                 System.out.println("-------------------------------------");
                 eleccionMenu = input.nextInt();
                 switch (eleccionMenu) {//Dependiendo del numero dado por el usuario ejecutamos el método correspondiente.
@@ -35,20 +38,53 @@ public class Tema11Ej6 {
                         System.out.println("Para usar esta opcion debe usar la opcion 1 primero.");
                     }
                 }
-                default -> System.out.println("Debes elegir una opcion del 1 al 4");
+                case 4 ->  {
+                    if (ready == true) {
+                    } else {
+                        System.out.println("Para usar esta opcion debe usar la opcion 1 primero.");
+                    }
+                }
+                case 5 ->  {
+                    if (ready == true) {
+                    } else {
+                        System.out.println("Para usar esta opcion debe usar la opcion 1 primero.");
+                    }
+                }
+                case 6 ->  {
+                    if (ready == true) {
+                    } else {
+                        System.out.println("Para usar esta opcion debe usar la opcion 1 primero.");
+                    }
+                }
+                default -> System.out.println("Debes elegir una opcion del 1 al 7");
             }
                 //Dependiendo del numero dado por el usuario ejecutamos el método correspondiente.
                             } catch (InputMismatchException e) {
                 System.out.println("Debe ser un número.");
                 input.nextLine();
-            } 
+            }  
             
-        } while (eleccionMenu != 4); //Volvemos a presentar el menu hasta que el usuario decide salir del programa.
+        } while (eleccionMenu != 7); //Volvemos a presentar el menu hasta que el usuario decide salir del programa.
 
+    }
+    
+    
+    
+    public static String pedirString() {
+        Scanner input = new Scanner(System.in);
+        String string = input.nextLine();
+        return string;
+    }
+    
+    public static int pedirInt() {
+        Scanner input = new Scanner(System.in);
+        int int1 = input.nextInt();
+        return int1;
     }
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        Campaña SIP = new Campaña("SIP");
+        menu(SIP);
     }
     
 }
